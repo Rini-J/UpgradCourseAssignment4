906cade47170d23feaafc475889b8b2c087be931
Rini-J <60174025+Rini-J@users.noreply.github.com>
2020-03-16 18:45:08 +0530

    Feature: Password Rule for New User Registration
    1.  Password entered by the user must contain at least 1 alphabet (a-z or A-Z), 1 number (0-9) and 1 special character (any character other than a-z, A-Z and 0-9).
    2.  After successful registration, you must directly return 'users/login.html' file.
    3. If Password Validation fails ,  user must not be registered and again return the ‘users/registration.html’ file. You also need to display a message ‘Password must contain at least 1 alphabet, 1 number & 1 special character’.

--- ImageHoster/src/main/java/ImageHoster/controller/UserController.java
+++ ImageHoster/src/main/java/ImageHoster/controller/UserController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 @Controller
@@ -40,10 +42,34 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
-        userService.registerUser(user);
-        return "redirect:/users/login";
+    public String registerUser(User user, Model model) {
+
+        String PasswordEntered = user.getPassword();
+
+        Boolean validationResult = PasswordCheck(PasswordEntered);
+        if (validationResult) {
+            userService.registerUser(user);
+            return "users/login";
+        } else {
+            user = new User();
+            UserProfile profile = new UserProfile();
+            user.setProfile(profile);
+            model.addAttribute("User", user);
+            String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
+            model.addAttribute("passwordTypeError", error);
+            return "users/registration";
+        }
     }
+
+
+
+    private Boolean PasswordCheck(String PasswordEntered) {
+        String PasswordRule = "((?=.*\\d)(?=.*[a-zA-Z])(?=.*[^a-zA-Z\\d]).{3,})";
+        Pattern pattern = Pattern.compile(PasswordRule);
+        Matcher matcher = pattern.matcher(PasswordEntered);
+        return matcher.matches();
+    }
+
 
     //This controller method is called when the request pattern is of type 'users/login'
     @RequestMapping("users/login")
